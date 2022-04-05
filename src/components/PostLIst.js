4fca670f90dedf5a8ef1cd3df45cfa39e95062f9
Daniel Vadil <dvadz@yahoo.com>
2022-04-05 16:38:40 -0400

    Used UserHeader in PostList and passed down usedId to it

--- src/components/PostLIst.js
+++ src/components/PostLIst.js
@@ -1,18 +1,36 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
+import UserHeader from "./UserHeader";
 
 class PostList extends React.Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
 
+  renderList() {
+    if (this.props.posts.length) {
+      return this.props.posts.map((post) => {
+        return (
+          <div className='item' key={post.id}>
+            <i className='large user middle aligned icon'></i>
+            <div className='content'>
+              <div className='header'>{post.title}</div>
+              <div className='description'>{post.body}</div>
+              <UserHeader useId={post.useId} />
+            </div>
+          </div>
+        );
+      });
+    }
+  }
+
   render() {
-    return <div>PostList</div>;
+    return <div className='ui relaxed divided list'>{this.renderList()}</div>;
   }
 }
 
 const mapStateToProps = (state) => {
-  return { post: state.posts };
+  return { posts: state.posts };
 };
 export default connect(mapStateToProps, { fetchPosts })(PostList);
