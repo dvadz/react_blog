1b83ccde91d813114e9316eb7a3d6902ade4bebc
Daniel Vadil <dvadz@yahoo.com>
2022-04-05 11:25:34 -0400

    Fixed typos

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postReducer";
 
 export default combineReducers({
-  placeholder: () => "Replace me",
+  posts: postsReducer,
 });
--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,8 +1,10 @@
-export default postReducer = (state = [], action) => {
+const postsReducer = (state = [], action) => {
   switch (action.type) {
-    case "FETCH_POSTS:":
+    case "FETCH_POSTS":
       return action.payload;
     default:
       return state;
   }
 };
+
+export default postsReducer;
