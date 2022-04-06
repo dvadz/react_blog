dd005eb906214aa80184409f1c523269255555ea
Daniel Vadil <dvadz@yahoo.com>
2022-04-06 10:35:20 -0400

    Used lodash memoize, refactored fetchUser to prevent duplicate queries for the same userId

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceholder from "../apis/jsonplaceholder";
+import _ from "lodash";
 
 export const fetchPosts = () => {
   return async (dispatch) => {
@@ -7,9 +8,23 @@ export const fetchPosts = () => {
   };
 };
 
+// KEPT FOR REFERENCE
+// Original fetchUser, executes axios for the same userId repeatedly
+// export const fetchUser = (id) => {
+//   return async (dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//     dispatch({ type: "FETCH_USER", payload: response.data });
+//   };
+// };
+
+// Used lodash memoize to prevent running duplicate queries for the same userId
 export const fetchUser = (id) => {
-  return async (dispatch) => {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
-    dispatch({ type: "FETCH_USER", payload: response.data });
+  return (dispatch) => {
+    _fetchUser(id, dispatch);
   };
 };
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({ type: "FETCH_USER", payload: response.data });
+});
